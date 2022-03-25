85c5248e3dcecf48f719443c2f7fc87b7c6e4e7f
John Hennerich <john.hennerich@gmail.com>
2022-03-24 19:16:33 -0600

    Add get/post/patch/detele routes to routes.db

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get '/', to: 'welcome#index'
   get '/winecellars', to: 'winecellars#index'
+  get '/winecellars/new', to: 'winecellars#new'
+  post '/winecellars' to: 'winecellars#create'
+  get '/winecellars/:id', to: 'winecellars#show'
+  get '/winecellars/:id/edit', to: 'winecellars#edit'
+  patch '/winecellars/:id', to: 'winecellars#update'
+  delete '/winecellars/:id', to: 'winecellars#destroy'
 end

a8fd6eaa025bd95a42f7ba89bc5d0e8596e3331f
John Hennerich <john.hennerich@gmail.com>
2022-03-24 19:39:14 -0600

    Add spec for show single winecellar for given id

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   get '/winecellars', to: 'winecellars#index'
   get '/winecellars/new', to: 'winecellars#new'
-  post '/winecellars' to: 'winecellars#create'
+  post '/winecellars', to: 'winecellars#create'
   get '/winecellars/:id', to: 'winecellars#show'
   get '/winecellars/:id/edit', to: 'winecellars#edit'
   patch '/winecellars/:id', to: 'winecellars#update'
---
+++ spec/features/user_sees_winecellar_id_spec.rb
@@ -0,0 +1,14 @@
+equire "rails_helper"
+
+describe "user sees a winecellar of a given id" do
+  describe "they visit /winecellars/id" do
+    it "displays a winecellar" do
+
+      john_1 = Winecellar.create!(name: 'John', full:0, location:'Basement', capacity:500)
+
+      visit '/winecellars/1'
+
+      expect(page).to have_content(john_1.name)
+    end
+  end
+end

a30bc5547b216970ab358df0531f5b80c8e5550c
John Hennerich <john.hennerich@gmail.com>
2022-03-27 16:25:23 -0600

    Feat: create new wine

--- spec/features/wines/wine_new_spec.rb
+++ spec/features/wines/wine_new_spec.rb
@@ -13,23 +13,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Wine creation' do
-    it 'links to the new page from the wine index' do
-      visit '/wines'
-      click_link('Create Wine')
-      expect(current_path).to eq('/wines/new')
-    end
+  it 'links to the new page from the wine index' do
+    visit '/wines'
+    click_link('Create Wine')
+    expect(current_path).to eq('/wines/new')
+  end
 
-    it 'can create a new winecellar' do
-      visit '/wines/new'
+  it 'can create a new winecellar' do
+    visit '/wines/new'
 
-      fill_in('Name', with: 'John wine')
-      fill_in('Varietal', with: 'Rose')
-      fill_in('Quantity', with: 1)
-      fill_in('Year', with: 2022)
-      fill_in('Wine Score', with: 90)
-      click_button('Create Wine Cellar')
+    fill_in('Name', with: 'John wine')
+    fill_in('Varietal', with: 'Rose')
+    fill_in('Quantity', with: 1)
+    fill_in('Year', with: Date.today.year)
+    fill_in('Wine Score', with: 90)
+    click_button('Create Wine Cellar')
 
-      expect(current_path).to eq("/wines")
-      expect(page).to have_content('John wine')
-
+    expect(current_path).to eq("/wines")
+    expect(page).to have_content('John wine')
+  end
 end

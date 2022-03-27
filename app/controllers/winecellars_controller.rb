30ab5678d7a257b9b2dea3d9fab5a48ddae728d0
John Hennerich <john.hennerich@gmail.com>
2022-03-27 10:36:19 -0600

    Feat: new winecellar logic

--- app/controllers/winecellars_controller.rb
+++ app/controllers/winecellars_controller.rb
@@ -4,7 +4,25 @@ class WinecellarsController < ApplicationController
   end
 
   def show
-    @winecellar = Winecellar.find(params[:id])
+    ### temperary fix for when id = new
+#    if params[:id] == "new"
+#      @winecellar = Winecellar.find(Winecellar.last.id)
+#    else
+      @winecellar = Winecellar.find(params[:id])
+#    end
     @winecount = Wine.number_of_wines(params[:id])
   end
+
+  def create
+    winecellar = Winecellar.create(winecellar_params)
+    redirect_to "/winecellars/#{winecellar.id}"
+  end
+
+  def winecellar_params
+    params.permit(:name, :location, :capacity)
+  end
+
+  def new
+  end
+
 end
